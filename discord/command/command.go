82b2a6ddfaefac16e707a78cc95bd89d01579d21
naoto.kido <naoto.kido@techblue.style>
2024-11-12 20:17:37 +0900

    コマンドコントローラーの作成

--- discord/command/command.go
+++ discord/command/command.go
@@ -1,10 +1,32 @@
 package command
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
 
 type Command interface {
 	GetName() string
-	Handler(s *discordgo.Session, m *discordgo.InteractionCreate)
-	Prefix(s *discordgo.Session, i *discordgo.MessageCreate)
-	Chat(s *discordgo.Session, i *discordgo.MessageCreate)
+	GetCommandData() *discordgo.ApplicationCommand
+	Handler(s *discordgo.Session, i *discordgo.InteractionCreate)
+	Prefix(s *discordgo.Session, m *discordgo.MessageCreate)
+}
+
+type Model struct {
+	Command
+	Detail Detail
+}
+
+type Detail struct {
+	name   string
+	prefix *string
+}
+
+func (d Detail) isCommand(i *discordgo.InteractionCreate) bool {
+	return i.ApplicationCommandData().Name == d.name
+}
+
+func (d Detail) isPrefix(s *discordgo.Session, m *discordgo.MessageCreate) bool {
+	return m.Author.ID != s.State.User.ID && strings.HasPrefix(m.Content, fmt.Sprintf("%s%s", *d.prefix, d.name))
 }
