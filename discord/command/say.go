18125bba5167fb3f7a2c3a04bc0676b4a9733ec1
naoto.kido <naoto.kido@techblue.style>
2025-01-09 16:18:03 +0900

    sayコマンドの実装

---
+++ discord/command/say.go
@@ -0,0 +1,67 @@
+package command
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
+
+type Say struct {
+	Detail
+}
+
+func NewSay(name string, prefix *string) *Say {
+	say := &Say{
+		Detail: Detail{
+			name:   name,
+			prefix: prefix,
+		},
+	}
+
+	say.Detail.Command = say
+
+	return say
+}
+
+func (say *Say) GetName() string {
+	return say.Detail.name
+}
+
+func (say *Say) GetCommandData() *discordgo.ApplicationCommand {
+	return &discordgo.ApplicationCommand{
+		Name:        say.GetName(),
+		Description: "say command",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "message",
+				Description: "message to say",
+				Required:    true,
+			},
+		},
+	}
+}
+
+func (say *Say) Handler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if !say.Detail.isCommand(i) {
+		return
+	}
+
+	option := i.ApplicationCommandData().Options[0].StringValue()
+	if option == "" {
+		interactionRespond(s, i, "空文字は送信できません")
+	}
+	interactionRespond(s, i, option)
+}
+
+func (say *Say) Prefix(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !say.Detail.isPrefix(s, m) {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, fmt.Sprintf("%v%s ", say.prefix, say.name)) {
+		s.ChannelMessageSendReply(m.ChannelID, strings.Replace(m.Content, fmt.Sprintf("%v%v ", say.prefix, say.name), "", 1), m.MessageReference)
+	} else {
+		s.ChannelMessageSendReply(m.ChannelID, fmt.Sprintf("%v%v { 内容 }", say.prefix, say.name), m.MessageReference)
+	}
+}
