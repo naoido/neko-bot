c754287410868d26de80b94e6c1ce33fb6d2bd60
naoto.kido <naoto.kido@techblue.style>
2025-01-09 15:37:17 +0900

    フォーラムの監視を動的に変更

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.23.2
 
 require (
 	github.com/bwmarrin/discordgo v0.28.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/term v0.26.0 // indirect
---
+++ redis/redis.go
@@ -0,0 +1,39 @@
+package redis
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	WatchedThreadIds string = "watched_thread_ids"
+	NoticeChannel    string = "notice_channel"
+)
+
+var instance *Cache
+
+type Cache struct {
+	ctx    context.Context
+	client *redis.Client
+}
+
+func Client() *redis.Client {
+	if instance == nil {
+		client := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		})
+
+		instance = &Cache{
+			ctx:    context.Background(),
+			client: client,
+		}
+	}
+
+	return instance.client
+}
+
+func Context() context.Context {
+	return instance.ctx
+}
